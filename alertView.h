08f153f9cc248215b49dd15f4da6318d6faa6b85
IvanKoskov <evanmatthewofficial@gmail.com>
2025-03-10 20:06:41 +0700

    UI

---
+++ alertView.h
@@ -0,0 +1,16 @@
+//
+//  alertView.h
+//  SXMac
+//
+//  Created by Evan Matthew on 10/3/2568 BE.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface alertView : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ alertView.m
@@ -0,0 +1,12 @@
+//
+//  alertView.m
+//  SXMac
+//
+//  Created by Evan Matthew on 10/3/2568 BE.
+//
+
+#import "alertView.h"
+
+@implementation alertView
+
+@end
