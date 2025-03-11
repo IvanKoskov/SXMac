06b09d31123520f93f9176d6f51d3f03a7f495d3
IvanKoskov <evanmatthewofficial@gmail.com>
2025-03-11 15:26:42 +0700

    UI improved, Seettings, TextEditor and more

---
+++ languagesView.swift
@@ -0,0 +1,18 @@
+//
+//  languagesView.swift
+//  SXMac
+//
+//  Created by Evan Matthew on 11/3/2568 BE.
+//
+
+import SwiftUI
+
+struct languagesView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    languagesView()
+}
