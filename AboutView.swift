87bab89ce87e0a63d81b48fc679c9e6f570bb315
IvanKoskov <evanmatthewofficial@gmail.com>
2025-03-12 16:56:18 +0700

    UI, About window, UI fixes

---
+++ AboutView.swift
@@ -0,0 +1,18 @@
+//
+//  AboutView.swift
+//  SXMac
+//
+//  Created by Evan Matthew on 12/3/2568 BE.
+//
+
+import SwiftUI
+
+struct AboutView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AboutView()
+}
