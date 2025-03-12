df5ca4ee55fd04e173dc5ea1c9d6d3ffdb00b933
IvanKoskov <evanmatthewofficial@gmail.com>
2025-03-12 14:08:55 +0700

    UI, Format searcher, File manager

---
+++ newFileView.swift
@@ -0,0 +1,18 @@
+//
+//  newFileView.swift
+//  SXMac
+//
+//  Created by Evan Matthew on 12/3/2568 BE.
+//
+
+import SwiftUI
+
+struct newFileView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    newFileView()
+}
