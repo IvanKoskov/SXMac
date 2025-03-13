8f64b38986a80c8f4d397fe4f03f03b286b67edc
IvanKoskov <evanmatthewofficial@gmail.com>
2025-03-13 10:22:16 +0700

    UI, WebView, README

---
+++ webPreviewView.swift
@@ -0,0 +1,18 @@
+//
+//  webPreviewView.swift
+//  SXMac
+//
+//  Created by Evan Matthew on 13/3/2568 BE.
+//
+
+import SwiftUI
+
+struct webPreviewView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    webPreviewView()
+}
