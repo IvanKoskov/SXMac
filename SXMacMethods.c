d1fcfe5df77680d5a1663211552c63f2d1079baa
IvanKoskov <evanmatthewofficial@gmail.com>
2025-03-18 09:20:16 +0700

    UI, settings support option

---
+++ SXMacMethods.c
@@ -0,0 +1,8 @@
+//
+//  SXMacMethods.c
+//  SXMac
+//
+//  Created by Evan Matthew on 18/3/2568 BE.
+//
+
+#include "SXMacMethods.h"
---
+++ SXMacMethods.h
@@ -0,0 +1,13 @@
+//
+//  SXMacMethods.h
+//  SXMac
+//
+//  Created by Evan Matthew on 18/3/2568 BE.
+//
+
+#ifndef SXMacMethods_h
+#define SXMacMethods_h
+
+#include <stdio.h>
+
+#endif /* SXMacMethods_h */
