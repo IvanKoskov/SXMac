8b9470f64cfb23b0230ab5f963eaac686347ceb5
IvanKoskov <evanmatthewofficial@gmail.com>
2025-03-25 16:18:58 +0700

    UI, Sorting

---
+++ libraries/sortByFileSize.h
@@ -0,0 +1,16 @@
+//
+//  sortByFileSize.h
+//  SXMac
+//
+//  Created by Evan Matthew on 25/3/25.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface sortByFileSize : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ libraries/sortByFileSize.m
@@ -0,0 +1,12 @@
+//
+//  sortByFileSize.m
+//  SXMac
+//
+//  Created by Evan Matthew on 25/3/25.
+//
+
+#import "sortByFileSize.h"
+
+@implementation sortByFileSize
+
+@end
