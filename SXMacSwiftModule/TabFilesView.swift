97dc18e9140f3c136ac7512dc08170acca2688d1
IvanKoskov <evanmatthewofficial@gmail.com>
2025-03-25 12:42:07 +0700

    UI, Windows, Text Editor

---
+++ SXMacSwiftModule/TabFilesView.swift
@@ -0,0 +1,18 @@
+//
+//  TabFilesView.swift
+//  SXMac
+//
+//  Created by Evan Matthew on 24/3/25.
+//
+
+import SwiftUI
+
+struct TabFilesView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TabFilesView()
+}
